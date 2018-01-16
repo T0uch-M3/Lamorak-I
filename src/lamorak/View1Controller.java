deb0c54689baa0b477f4547f6a29be643a0dcf2d
Touch-Me <Touch-Me@192.168.1.3>
2018-01-17 00:21:33 +0100

    Logging added (kinda)

--- src/lamorak/View1Controller.java
+++ src/lamorak/View1Controller.java
@@ -15,9 +15,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import java.lang.Exception;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -26,43 +27,82 @@ import java.util.logging.Logger;
  */
 public class View1Controller extends AnchorPane implements Initializable{
     
+    PreparedStatement st = null;
+    String sql2 = "SELECT * FROM EMPLOYEE";
+    ArrayList<String> store = new ArrayList<>();
     @FXML
     private TextField name;
     private PasswordField password;
     @FXML
-    private void ConfirmAction (ActionEvent event) throws Exception {
-        //if ("test".equals(name.getText())){
-        if ("test".equals(name.getText())){
+    private void ConfirmAction (ActionEvent event) throws Exception{
+        try {
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Project","Test","test");
+            System.out.println("Connected");
+            st = con.prepareStatement(sql2);
+            ResultSet result = st.executeQuery();
+            while(result.next()) {
+             if ((result.getBoolean(3)==true)&&(result.getString(2).equals(name.getText()))) 
+                 //store.add(result.getString(2));
+                 this.move();
+             else
+                 System.out.println("wrong choice mate");
+//             System.out.println(result.getString    (1));//id
+//             System.out.println(result.getString    (2));//name
+//             System.out.println(result.getBoolean   (3));//VIP or no
+//             System.out.println(result.getString    (4));//password
+            }
+//            System.out.println(store);
+//             if ("test".equals(name.getText())){
+//             this.move();
+//             }
+        } catch (SQLException ex){
+            System.out.println("It got fucked");
+            Logger.getLogger(View1Controller.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            st.close();
+        }
+    }
+    
+    private void move() throws Exception{
         Main m = new Main();
         m.goto2();
-        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources)  {
-        String insertTableSQL = "INSERT INTO EMPLOYEE"
-				+ "(ID, NAME, VIP, PWD) VALUES"
-				+ "(?,?,?,?)";
-        PreparedStatement st = null;
-        try {
-            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Project","Test","test");
-            System.out.println("Connected");
-            st = con.prepareStatement(insertTableSQL);
-            st.setString(1, "4");
-            st.setString(2, "aaa");
-            st.setBoolean(3, false);
-            st.setString(4, null);
-            st.executeUpdate();
-        } catch (SQLException ex) {
-            System.out.println("It got fucked");
-            Logger.getLogger(View1Controller.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                st.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(View1Controller.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+//        String sql1 = "INSERT INTO EMPLOYEE"
+//				+ "(ID, NAME, VIP, PWD) VALUES"
+//				+ "(?,?,?,?)";
+//        //String sql2 = "SELECT * FROM EMPLOYEE";
+//        //PreparedStatement st = null;
+//        try {
+//            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Project","Test","test");
+//            System.out.println("Connected");
+//            st = con.prepareStatement(sql2);
+//            ResultSet result = st.executeQuery();
+//            while(result.next()) {
+//
+//             System.out.println(result.getString    (1));
+//             System.out.println(result.getString    (2));
+//             System.out.println(result.getBoolean   (3));
+//             System.out.println(result.getString    (4));
+//            }
+//
+////            st.setString(1, "4");
+////            st.setString(2, "aaa");
+////            st.setBoolean(3, false);
+////            st.setString(4, null);
+////            st.executeUpdate();
+//        } catch (SQLException ex) {
+//            System.out.println("It got fucked");
+//            Logger.getLogger(View1Controller.class.getName()).log(Level.SEVERE, null, ex);
+//        } finally {
+//            try {
+//                st.close();
+//            } catch (SQLException ex) {
+//                Logger.getLogger(View1Controller.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
     }
     
 }
