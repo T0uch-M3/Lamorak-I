930ac5be6d28b8d9e11e1517e6b532e18c384d97
Touch-Me <Touch-Me@192.168.1.3>
2021-07-05 12:14:10 +0100

    Fix branches

--- src/LM/View2Controller.java
+++ src/LM/View2Controller.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lamorak;
+package LM;
 
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXRadioButton;
